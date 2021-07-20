c5e3c19504356c2014141383f9be97f1e9d0de4e
leo <realleo2009@hotmail.com>
2021-07-20 17:44:26 -0300

    8-7

--- lib/views/product_overview_screen.dart
+++ lib/views/product_overview_screen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import 'package:shop/models/product.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (context, i) => Text(loadedProducts[i].title),
+        itemCount: loadedProducts.length,
+        padding: const EdgeInsets.all(10),
+      ),
+    );
   }
 }
